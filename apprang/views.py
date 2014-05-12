bbef44638c8d457508b32aee1c44325a242a3f07
AcmeProgramming <AcmeProgramming@users.noreply.github.com>
2014-05-12 00:15:55 -0400

    Created admin, models, views information. Modified mainrango urls, requirements.txt.  New directories media, static, templates

--- apprang/views.py
+++ apprang/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.http import HttpResponse
 
+
+
 def index(request):
-    return HttpResponse("this is output")
+    context = RequestContext(request)
+    context_dict = {'boldmessage': "Content from context_dict in index def apprang.views"}
+    return render_to_response('apprang/index.html', context_dict, context)
+
+def about(request):
+    about_context = RequestContext(request)
+    about_context_dict = {'boldmessage': "This is bold messaging information from about def"}
+    return render_to_response('apprang/about.html', about_context_dict, about_context)
--- mainrango/urls.py
+++ mainrango/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -15,6 +16,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    ##url(r'^apprang/', include('apprang.urls')),
-    url(r'^$', include('apprang.urls')),
+    url(r'^apprang/', include('apprang.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}), 
+    )
